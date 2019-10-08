c3a1432489e4efe545c43c6c82c95c2ea248098a
mickymck <mickymckeon@gmail.com>
2019-10-07 21:33:17 -0400

    got a few functions that do their job, but too many loops

--- word_frequency.py
+++ word_frequency.py
@@ -1,17 +1,39 @@
+
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+def remove_stop_words(text):
+    final_word_list = []
+    for word in text:
+        if word not in STOP_WORDS:
+            final_word_list.append(word)
+            print(final_word_list)
+
+def remove_punctuation(text):
+    for character in text:
+        if character in string.punctuation:
+            text = text.replace(character, "")
+            word_list = text.split()
+            return word_list
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
 
-with open ("seneca_falls.txt") as file:
-    print(file.read())
+    with open (file) as file:
+        text = file.read()
+        lowercase = text.lower()
+        # list_items = lowercase.split()
+        unpunctuated = remove_punctuation(lowercase)
+        remove_stop_words(unpunctuated)
 
+
+
+print_word_freq("seneca_falls.txt")
 
 if __name__ == "__main__":
     import argparse
